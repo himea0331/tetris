e6a7368ee40fc7877534fc3078b5a02e09dc56a9
unknown <>
2016-04-21 00:16:27 +0900

    クラス追加

---
+++ Tetris/Tetris/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Controller
+    {
+        // Blockが1列にそろった場合削除
+
+        // キーボード操作
+
+    }
+}
